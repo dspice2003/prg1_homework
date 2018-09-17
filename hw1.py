e8f64ad661368612a9a3de6cbd0e1f2495571b94
dspice2003 <dspice@sjnma.org>
2018-09-16 18:36:43 -0600

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,1 +1,22 @@
+'''
+problem 1
+
+'''
+a = 15 
+b = 1.02
+c = True
+d = "hello world"
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+'''
+problem 2
+
+'''
+A = float(input("please enter a number for a: "))
+
+# square  root of 4**(1/2) 4 1/2
 
