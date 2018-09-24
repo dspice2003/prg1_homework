f12e19606ebdabf96c7ea8a950dcd21604e9cda3
dspice2003 <dspice@sjnma.org>
2018-09-23 18:51:58 -0600

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,1 +1,59 @@
+'''
+problem 1
 
+'''
+
+camping_people = int(input("How many people will be attending: ")
+
+if camping_people == 1:
+    print("Just you then?")
+
+if camping_people == 2:
+    print("Bring marshmallows, please!")
+
+if camping_people >=6:
+    print("More than three is a crowd!")
+
+if camping_people >= 7:
+    print("its a party!")
+
+
+    '''
+    problem 2
+    
+    '''
+
+    
+    print("you are driving to fast!")
+    speed_limit = 15
+    
+    print("what is the speed limit?"))
+    print(speed_limit)
+
+    speed_limit_question = int(input("how fast were you going!"))
+    birthday = str(input("Is it your birthday?"))
+
+    if speed_limit_question <= speed_limit:
+        print("no ticket!")
+
+        if birthday == "yes":
+            print(speed_limit_question - 5)
+
+    if speed_limit_question > speed_limit:
+        print("warning!")
+
+        if birthday == "yes":
+            print(speed_limit_question - 5)
+
+    if speed_limit_question >= 5:
+        print("small ticket!")
+        if birthday == "yes"
+            print("warning")
+        else:
+            print("Small ticket!")
+
+    if speed_limit_question > speed_limit:
+        print("big ticket!")
+
+        if birthday == "yes":
+            print(speed_limit_question - 5)
