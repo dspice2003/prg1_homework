6714272199c4f87946d8d417bcec99e8d9157525
dspice2003 <dspice@sjnma.org>
2018-10-25 14:34:10 -0600

    for_loops_quiz done

---
+++ practice_code/class.py
@@ -0,0 +1,9 @@
+class_list = ["garza","walmsley",]
+
+for student in class_list:
+    print(student)
+    upper_student = student.upper()
+print("done")
+print()
+print(upper_student)
+print()
---
+++ practice_code/for.py
@@ -0,0 +1,7 @@
+number = input("Please enter a number: ")
+
+sum = 0
+
+for number_list in number:
+    number.split("+")
+    print(number)
